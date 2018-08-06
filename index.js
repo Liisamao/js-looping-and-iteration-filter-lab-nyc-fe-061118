1d3d07852930c5ae96c4be7cbdca1657a00c910a
Lisa Mao <liisamao@gmail.com>
2018-08-06 23:24:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file
+function findMatching (arr,name){
+  return arr.filter( n => name.toLowerCase() === n.toLowerCase() );
+}
+
+function fuzzyMatch (arr, name){
+  return arr.filter( n => name[0] === n[0]);
+}
+
+
+// [,{name: "Sophie"} {name: "Lisa"}]
+function matchName (arr, name){
+  return arr.filter(person => person.name === name);
+}
