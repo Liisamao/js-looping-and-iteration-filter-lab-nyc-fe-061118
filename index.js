8a1344262d5a46e401727c11237a9dd772902481
Lisa Mao <liisamao@gmail.com>
2018-08-06 23:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching (arr,name){
+  return arr.filter( n => name.toLowerCase() === n.toLowerCase() );
+}
+
+function fuzzyMatch (arr, name){
+  return arr.filter( n => name[0] === n[0]);
+}
+
+function matchName (arr, name){
+  return arr.filter( n => name === n);
+}
